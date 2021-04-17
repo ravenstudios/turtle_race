1b34e7abbca75cba2021b243b1cfb7775f7ba0f8
ravenstudios <ravenstudiosrob@yahoo.com>
2021-04-16 21:45:33 -0500

    finished racing

--- main.py
+++ main.py
@@ -12,13 +12,14 @@ pygame.init()
 turtles_group = 0
 
 
-
+running = True
+start_race = False
 
 
 
 
 def main():
-    running = True
+    global running, start_race
 
     while running:
         clock.tick(TICK_RATE)
@@ -40,11 +41,15 @@ def main():
             if event.type == pygame.KEYUP:
                 if event.key == 32:#SPACE
                     # updates the bomb group when you set a bomb
+                    if start_race == False:
+                        for t in turtles_group:
+                            t.start()
+                    start_race = True
 
-                    pass
 
         draw()
-        update()
+        if start_race == True:
+            update()
 
     pygame.quit()
     sys.exit()
@@ -52,7 +57,7 @@ def main():
 
 
 def draw():
-    surface.fill((100, 100, 100))#background
+    surface.fill((50, 50, 50))#background
     turtles_group.draw(surface)
 
 
@@ -61,13 +66,30 @@ def draw():
 
 
 def update():
-
+    global start_race
     turtles_group.update()
 
+    if end_race():
+        start_race = False
+        for t in turtles_group:
+            print(t.get_results())
+
+def end_race():
+
+    for t in turtles_group:
+        if t.ended_race == False:
+            return False
+
+    return True
 
 if __name__ == "__main__":
 
 
     turtles_group = pygame.sprite.Group()
-    turtles_group.add(turtle.Turtle(100, RED))
+    turtles_group.add(turtle.Turtle(1, 100, RED))
+    turtles_group.add(turtle.Turtle(2, 200, GREEN))
+    turtles_group.add(turtle.Turtle(3, 300, WHITE))
+    turtles_group.add(turtle.Turtle(4, 400, BLUE))
+    turtles_group.add(turtle.Turtle(5, 500, (80, 59, 43)))
+
     main()
--- turtle.py
+++ turtle.py
@@ -1,5 +1,5 @@
 from constants import *
-import pygame
+import pygame, random, datetime
 
 class Turtle(pygame.sprite.Sprite):
 
@@ -7,8 +7,9 @@ class Turtle(pygame.sprite.Sprite):
         # TODO:
 
     """
-    def __init__(self, y, color):
+    def __init__(self, name, y, color):
         super().__init__()
+        self.name = name
         self.width = BLOCK_SIZE
         self.height = BLOCK_SIZE
         self.x = 100
@@ -19,7 +20,46 @@ class Turtle(pygame.sprite.Sprite):
         self.rect = pygame.Rect(self.image.get_rect())
         self.rect.topleft = (self.x, self.y)
         self.speed = 5
+        self.rdm_min = 0
+        self.rdm_max = 3
+
+        self.can_start = False
+
+        self.ended_race = False
+        self.start_time = 0
+        self.end_time = 0
+        self.top_speed = 0
+
 
 
     def update(self):
-        self.rect.left += self.speed
+
+        if self.can_start:
+
+            speed = self.speed * random.randint(self.rdm_min, self.rdm_max)
+
+            if speed > self.top_speed:
+                self.top_speed = speed
+
+            self.rect.left += speed
+
+        if self.rect.right > FINISH_LINE:
+            self.ended_race = True
+            self.rect.left = FINISH_LINE
+            self.end_time = datetime.datetime.now()
+            self.can_start = False
+
+
+
+    def start(self):
+        self.start_time = datetime.datetime.now()
+        self.can_start = True
+
+    def get_results(self):
+        results = {
+            "name": self.name,
+            "start": self.start_time,
+            "end": self.end_time,
+            "top_speed": self.top_speed
+        }
+        return results
